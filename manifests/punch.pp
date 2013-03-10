ba6f3be9240a6a9445d3f864ad0ef1de6dfc3b3a
Michael Cheng <mcheng.work@gmail.com>
2013-03-10 12:29:03 +0800

    Add Punch specific config.

--- manifests/punch.pp
+++ manifests/punch.pp
@@ -4,4 +4,9 @@ class punch{
 		creates => "/usr/local/lib/node_modules/punch",
 		require => Class["nodejs"]
 	}
+	exec { "install-punch-blog":
+		command => "/usr/local/bin/npm install -g punch-blog-content-handler",
+		creates => "/usr/local/lib/node_modules/punch-blog-content-handler",
+		require => Class["nodejs"]
+	}
 }
